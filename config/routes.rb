5d087d9555a2c087c8ec7ac11bcab7696a837b9f
rnjai <rnjailamba12@gmail.com>
2015-01-27 00:31:33 +0530

    Simple authentication added,console is good

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+
 
   devise_for :users, controllers: { sessions: "users/sessions",registrations: "users/registrations",confirmations: "users/confirmations",passwords: "users/passwords",unlocks: "users/unlocks" }
   get  'static_pages/home'
