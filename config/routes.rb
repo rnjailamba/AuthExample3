dc8386437cafd7d33891d9bf2734df9b18e3840a
rnjai <rnjailamba12@gmail.com>
2015-01-27 23:39:49 +0530

    Added active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users, controllers: { sessions: "users/sessions",registrations: "users/registrations",confirmations: "users/confirmations",passwords: "users/passwords",unlocks: "users/unlocks" }
   get  'static_pages/home'
 
